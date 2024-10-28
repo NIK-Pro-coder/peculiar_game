1f3172e83f1ce77b6460465d865a1d3beace0296
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2024-10-28 21:06:27 +0100

    Added images

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ let giventags;
 let tag1, tag2;
 let round = 0;
 let score = 0;
+let basetag;
 
 function formatTag(tag) {
 	let val = tag.replace(/_/g, " ");
@@ -9,7 +10,7 @@ function formatTag(tag) {
 	return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
 
-function loadTags() {
+async function loadTags() {
 	round++;
 
 	let buttontag1 = document.getElementById("tag1");
@@ -23,13 +24,19 @@ function loadTags() {
 	buttontag1.innerHTML = formatTag(tag1.tag);
 	buttontag2.innerHTML = formatTag(tag2.tag);
 
-	let posttag1 = document.getElementById("post1");
-	let posttag2 = document.getElementById("post2");
-
-	posttag1.innerHTML = `${tag1.val} posts`;
-	posttag2.innerHTML = `${tag2.val} posts`;
-
 	document.getElementById("roundNum").innerHTML = `Round ${round}`;
+
+	let imgtag1 = document.getElementById("imgtag1");
+	let imgtag2 = document.getElementById("imgtag2");
+
+	let img1 = (await postBy(`${basetag}+${tag1.tag}`))[0];
+	let img2 = (await postBy(`${basetag}+${tag2.tag}`))[0];
+
+	imgtag1.src = img1.preview_url;
+	imgtag2.src = img2.preview_url;
+
+	imgtag1.style.height = `calc(${img1.heigth}px / 40%)`;
+	imgtag2.style.height = `calc(${img2.heigth}px / 40%)`;
 }
 
 function randomProperty(obj) {
@@ -40,12 +47,20 @@ function randomProperty(obj) {
 	};
 }
 
-async function start() {
-	const tag = document.getElementById("tag").value;
-
+async function postBy(tag) {
 	const url = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=${tag}&json=1&limit=1000`;
 	const data = await fetch(url);
 	const posts = await data.json();
+
+	return posts;
+}
+
+async function start() {
+	const tag = document.getElementById("tag").value;
+
+	basetag = tag;
+
+	const posts = await postBy(tag);
 
 	let child = {};
 
@@ -93,10 +108,16 @@ function tryTag(num) {
 
 	document.getElementById("posts").classList.toggle("hidden");
 
+	let htag1 = document.getElementById("tag1");
+	let htag2 = document.getElementById("tag2");
+
+	htag1.innerHTML = `${htag1.innerHTML} | ${tag1.val} posts`;
+	htag2.innerHTML = `${htag2.innerHTML} | ${tag2.val} posts`;
+
 	revealed = true;
 
-	let buttontag1 = document.getElementById("tag1");
-	let buttontag2 = document.getElementById("tag2");
+	let buttontag1 = document.getElementById("imgtag1");
+	let buttontag2 = document.getElementById("imgtag2");
 
 	if (tag1.val > tag2.val) {
 		buttontag1.classList.add("correct");
@@ -121,11 +142,13 @@ function tryTag(num) {
 }
 
 function next() {
+	if (!revealed) return;
+
 	document.getElementById("posts").classList.toggle("hidden");
 	loadTags();
 
-	let buttontag1 = document.getElementById("tag1");
-	let buttontag2 = document.getElementById("tag2");
+	let buttontag1 = document.getElementById("imgtag1");
+	let buttontag2 = document.getElementById("imgtag2");
 
 	buttontag1.classList.remove("correct", "equal", "wrong");
 	buttontag2.classList.remove("correct", "equal", "wrong");
